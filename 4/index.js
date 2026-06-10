52677cd78c5b7e7278825fdd6b5ac1156fce1b6d
Kauan Domingos <kauan.rocha@ondacom.com>
2026-06-10 17:38:36 -0300

    estudos de hoje.

---
+++ 4/index.js
@@ -0,0 +1,21 @@
+const produtos = [
+  { id: 1, nome: "Mouse Gamer", preco: 120.00, categoria: "Periféricos" },
+  { id: 2, nome: "Teclado Mecânico", preco: 350.00, categoria: "Periféricos" },
+  { id: 3, nome: "Monitor LED 24\"", preco: 890.00, categoria: "Hardware" },
+  { id: 4, nome: "Mousepad", preco: 45.00, categoria: "Periféricos" },
+  { id: 5, nome: "Cabo HDMI", preco: 25.00, categoria: "Acessórios" },
+  { id: 6, nome: "Headset", preco: 190.00, categoria: "Periféricos" },
+  { id: 7, nome: "Adaptador USB-C", preco: 60.00, categoria: "Acessórios" }
+];
+
+
+const formatarProdutos = (produtos) => {
+    return produtos.map(item => ({
+        ...item,
+        precoFormatado: "R$: " + item.preco
+    }))
+}
+
+
+console.log(formatarProdutos(produtos));
+
