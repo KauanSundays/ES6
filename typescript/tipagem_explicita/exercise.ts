5bc2a6a804d46944d7da91bb298e778bf34cab94
Kauan Domingos <kauan.rocha@ondacom.com>
2026-06-24 11:03:38 -0300

    Tipagem explicita e inferencia de tipo exercicio feito.

---
+++ typescript/tipagem_explicita/exercise.ts
@@ -0,0 +1,12 @@
+let nomeLoja = 'Pizzaria do bairro'
+let endereco: string;
+
+function calcularTotal (precoItem: number, taxaEntrega: number): number {
+    let totalApagar: number;
+    totalApagar = precoItem + taxaEntrega;
+
+    
+    return totalApagar
+}
+
+console.log(calcularTotal(40, 5));
