dbf2f6e3ec57371df86009cdad21bbff3cfe5b26
Kauan Domingos <kauan.rocha@ondacom.com>
2026-07-01 11:35:42 -0300

    :sparkles: feat: Type aliases.

---
+++ typescript/type_aliases/index.ts
@@ -0,0 +1,13 @@
+type FranquiaNBA = {
+    nome: string,
+    conferencia: "Leste" | "Oeste",
+    titulos: number,
+    principaisJogadores: string[]
+}
+
+const meuTime: FranquiaNBA = {
+    nome: "Boston Celtics",
+    conferencia: "Leste",
+    titulos: 18,
+    principaisJogadores: ["Tatum", "J. Brown"]
+}
