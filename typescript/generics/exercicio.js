cf28127e765de2ae109a610e58b1f638ab1c6ca9
Kauan Domingos <kauan.rocha@ondacom.com>
2026-07-10 15:37:36 -0300

    :sparkles: feat: Generics exercicio.

---
+++ typescript/generics/exercicio.js
@@ -0,0 +1,17 @@
+"use strict";
+// 2. Complete a função usando Generics (<T>)
+function fazerSubstituicao(jogadorSaindo, jogadorEntrando) {
+    console.log(`Sai: ${jogadorSaindo.nome} 🔄 Entra: ${jogadorEntrando.nome}`);
+    return jogadorEntrando;
+}
+const goleiroTitular = { posicao: "goleiro", nome: "Alisson", defesas: 5 };
+const goleiroReserva = { posicao: "goleiro", nome: "Ederson", defesas: 2 };
+const atacanteTitular = { posicao: "atacante", nome: "Vini Jr", gols: 15 };
+const atacanteReserva = { posicao: "atacante", nome: "Rodrygo", gols: 8 };
+// ✅ Isso deve funcionar perfeitamente:
+const novoGoleiro = fazerSubstituicao(goleiroTitular, goleiroReserva);
+// O TypeScript deve saber que 'novoGol eiro' é do tipo Goalkeeper e tem o atributo .defesas
+console.log(novoGoleiro.defesas);
+// ❌ Isso DEVE DAR ERRO no TypeScript se você tentar compilar:
+// O técnico não pode colocar um atacante no lugar de um goleiro usando essa função!
+const erroSubstituicao = fazerSubstituicao(atacanteTitular, atacanteReserva);
