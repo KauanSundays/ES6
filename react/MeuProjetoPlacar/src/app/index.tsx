60726cf306d94f254fc9aa36a14cad437fd7953b
Kauan Domingos <kauan.rocha@ondacom.com>
2026-07-29 08:33:10 -0300

    Projeto meu placar NFL usando o useState Hook.

--- react/MeuProjetoPlacar/src/app/index.tsx
+++ react/MeuProjetoPlacar/src/app/index.tsx
@@ -3,6 +3,11 @@ import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 export default function DriveNFL() {
   // TODO 1: Crie o estado para 'nomeQB' (Começa como string vazia ou "Mahomes")
+  const [nomeQB, setNomeQB] = useState("")
+  const [linhaDeJardas, setJardas] = useState(20)
+  const [descida, setDescidas] = useState(1)
+  const [isTouchdown, setTouchdown] = useState(false)
+  const [status, setStatus] = useState("Avançando no campo")
   
   // TODO 2: Crie o estado para 'linhaDeJardas' (Começa na linha de 20 jardas)
 
@@ -12,7 +17,36 @@ export default function DriveNFL() {
 
 
   // TODO 5: Crie a função para mudar de descida (Aumenta +1, com trava para não passar de 4)
+  const mudarDescida = () => {
+    if (descida < 4) {
+        setDescidas(descida + 1);
+    } else {
+        setDescidas(1);
+    }
+  }
 
+  const passeCurto = () => {
+    if (linhaDeJardas < 95) {
+        setJardas(linhaDeJardas + 5);
+    } else {
+        marcarTouchdown()
+    }
+  }
+
+  const passeApimentado = () => {
+    setDescidas(1);
+
+    if (linhaDeJardas < 80) {
+        setJardas(linhaDeJardas + 20);
+    } else {
+        marcarTouchdown()
+    }
+  }
+
+  const marcarTouchdown = () => {
+    setJardas(100);
+    setStatus("TOUCHDOWN EAGLES")
+  }
 
   return (
     <View style={styles.container}>
@@ -23,24 +57,25 @@ export default function DriveNFL() {
         style={styles.input}
         placeholder="Digite o nome do Quarterback..."
         placeholderTextColor="#888"
+        onChangeText={setNomeQB}
         // TODO 6: Conecte o valor (value) e o onChangeText com seu estado de nomeQB
       />
 
       {/* Card do Placard / Drive */}
       <View style={styles.card}>
         <Text style={styles.qbTexto}>
-          QB: {/* Exiba o nome do Quarterback aqui */}
+          QB: {nomeQB}
         </Text>
         <Text style={styles.downTexto}>
-          Descida Atual: {/* Exiba a descida (1, 2, 3 ou 4) */}ª Descida (Down)
+          Descida Atual: {descida}ª Descida (Down)
         </Text>
         <Text style={styles.jardasTexto}>
-          Posição no Campo: Linha de {/* Exiba a linhaDeJardas */} jardas
+          Posição no Campo: Linha de {linhaDeJardas} jardas
         </Text>
         
         {/* Status de Touchdown: Se isTouchdown for true, exiba "🎉 TOUCHDOWN!", senão "🏈 Luta por jardas" */}
         <Text style={styles.statusTexto}>
-          Status: {/* Renderize a mensagem baseada no boolean 'isTouchdown' */}
+          Status: {status}
         </Text>
       </View>
 
@@ -48,9 +83,7 @@ export default function DriveNFL() {
       <View style={styles.acoes}>
         <Button 
           title="Passe Curtinho (+5 Jardas)" 
-          onPress={() => {
-            /* TODO 7: Soma +5 na linhaDeJardas */
-          }} 
+          onPress={passeCurto} 
         />
         
         <View style={{ height: 10 }} />
@@ -58,9 +91,7 @@ export default function DriveNFL() {
         <Button 
           title="Passe Apimentado! 🚀 (+20 Jardas)" 
           color="#e67e22"
-          onPress={() => {
-            /* TODO 8: Soma +20 na linhaDeJardas */
-          }} 
+          onPress={passeApimentado} 
         />
 
         <View style={{ height: 10 }} />
@@ -68,9 +99,9 @@ export default function DriveNFL() {
         <Button 
           title="Próxima Descida (Avançar Down)" 
           color="#f1c40f"
-          onPress={() => {
-            /* TODO 9: Chame a função que aumenta a descida sem passar de 4 */
-          }} 
+          onPress={
+            mudarDescida
+          } 
         />
 
         <View style={{ height: 10 }} />
@@ -78,9 +109,7 @@ export default function DriveNFL() {
         <Button 
           title="Marcar Touchdown! 🏆" 
           color="#2ecc71"
-          onPress={() => {
-            /* TODO 10: Atualiza a linhaDeJardas para 100 e isTouchdown para true */
-          }} 
+          onPress={marcarTouchdown} 
         />
       </View>
     </View>
