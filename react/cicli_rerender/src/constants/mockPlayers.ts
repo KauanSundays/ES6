421020cf42c98b76f8afa85d42d8d5f9eb2960a7
Kauan Domingos <kauan.rocha@ondacom.com>
2026-08-11 16:53:37 -0300

    :sparkles: feat: Mock, adicionando estados.

---
+++ react/cicli_rerender/src/constants/mockPlayers.ts
@@ -0,0 +1,46 @@
+import { Player } from "../types/index";
+
+export const PLAYERS_DATA: Player[] = [
+  {
+    id: 1,
+    name: "Jalen Hurts",
+    position: "QB",
+    price: 25000000,
+    isDrafted: false
+  },
+  {
+    id: 2,
+    name: "Sidney Crosby",
+    position: "C",
+    price: 8700000,
+    isDrafted: false
+  },
+  {
+    id: 3,
+    name: "Jason Kelce",
+    position: "C",
+    price: 14000000,
+    isDrafted: false
+  },
+  {
+    id: 4,
+    name: "Evgeni Malkin",
+    position: "C",
+    price: 6100000,
+    isDrafted: false
+  },
+  {
+    id: 5,
+    name: "Neymar Jr.",
+    position: "ATA",
+    price: 30000000,
+    isDrafted: false
+  },
+  {
+    id: 6,
+    name: "Pelé",
+    position: "ATA",
+    price: 50000000,
+    isDrafted: false
+  }
+];
--- react/cicli_rerender/src/hooks/usePlayers.ts
+++ react/cicli_rerender/src/hooks/usePlayers.ts
@@ -1,46 +1,10 @@
-import { Player } from "../types/index";
+import { Player } from "../types/"
+import { PLAYERS_DATA } from "../constants/mockPlayers"
+import { useState, useEffect } from "react"
 
-export const PLAYERS_DATA: Player[] = [
-  {
-    id: 1,
-    name: "Jalen Hurts",
-    position: "QB",
-    price: 25000000,
-    isDrafted: false
-  },
-  {
-    id: 2,
-    name: "Sidney Crosby",
-    position: "C",
-    price: 8700000,
-    isDrafted: false
-  },
-  {
-    id: 3,
-    name: "Jason Kelce",
-    position: "C",
-    price: 14000000,
-    isDrafted: false
-  },
-  {
-    id: 4,
-    name: "Evgeni Malkin",
-    position: "C",
-    price: 6100000,
-    isDrafted: false
-  },
-  {
-    id: 5,
-    name: "Neymar Jr.",
-    position: "ATA",
-    price: 30000000,
-    isDrafted: false
-  },
-  {
-    id: 6,
-    name: "Pelé",
-    position: "ATA",
-    price: 50000000,
-    isDrafted: false
-  }
-];
+export const usePlayers = () => {
+    const [players, setPlayers] = useState<Player[]>([]);
+    const loading = useState<boolean>(true);
+    const error = useState<string | null>(null)
+}
+
