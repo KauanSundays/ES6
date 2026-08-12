7feef2679f099b5ee8775fce0a22bc08e4616a10
Kauan Domingos <kauan.rocha@ondacom.com>
2026-08-12 08:16:01 -0300

    :sparkles: feat: Etapa 2.

--- react/cicli_rerender/src/hooks/usePlayers.ts
+++ react/cicli_rerender/src/hooks/usePlayers.ts
@@ -4,21 +4,34 @@ import { useState, useEffect } from "react"
 
 export const usePlayers = () => {
     const [players, setPlayers] = useState<Player[]>([]);
-    const loading = useState<boolean>(true);
-    const error = useState<string | null>(null)
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    async function fetchPlayers(): Promise<Player[]> {
+        const response = PLAYERS_DATA;
+        return response;
+    }
+
+
+    useEffect(() => {
+        const timer = setTimeout( async () => {
+            try {
+                const response = await fetchPlayers();
+                setPlayers(response);
+            } catch (err) {
+                setError("Não foi possível carregar os atletas")
+            } finally {
+                setLoading(false)
+            }
+        }, 1500);
+
+        return () => clearTimeout(timer)
+    }, []);
+
+    return {
+        players, 
+        loading,
+        error,
+    }
 }
 
-
-useEffect(() => {
-    const timer = setTimeout(() => {
-        try {
-            
-        } catch (err) {
-
-        } finally {
-
-        }
-    }, 1500);
-
-    return () => clearTimeout(timer)
-})
